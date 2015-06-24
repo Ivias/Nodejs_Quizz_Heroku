80f4103573f0e5319ca1e6029d820104983671de
Ivias.Aren <ivias.aren@gmail.com>
2015-06-24 22:10:39 +0200

    Buscador de Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -16,11 +16,19 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
+if(req.query.search) {
+    var filtro  = (req.query.search || '').replace(" ", "%");
+    models.Quiz.findAll({where:["pregunta like ?", '%'+filtro+'%'],order:'pregunta ASC'}).then(function(quizes){
+      res.render('quizes/index', {quizes: quizes});
+    }).catch(function(error) { next(error);});
+
+  } else {
   models.Quiz.findAll().then(
     function(quizes) {
       res.render('quizes/index', { quizes: quizes});
     }
   ).catch(function(error) { next(error);})
+ }
 };
 
 // GET /quizes/:id
@@ -36,6 +44,7 @@ exports.answer = function(req, res) {
   }
   res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
+
 
 //GET /autor
 exports.autor = function(req,res) {
