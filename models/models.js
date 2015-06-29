df6cec92dd782bf407406558cf60c4ee659264e3
Ivias.Aren <ivias.aren@gmail.com>
2015-06-29 16:14:38 +0200

    Añadir Comentalrios

--- models/models.js
+++ models/models.js
@@ -28,11 +28,20 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 				}
 			);
 
+
 //Importar la definicion de la tabla Quiz en quiz.js
 var Quiz= sequelize.import(path.join(__dirname,'quiz'));
 
-exports.Quiz = Quiz; //Exportar la definicon de la tabla Quiz
 
+// Importar definicion de la tabla Comment
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
+exports.Quiz = Quiz; //Exportar la definicon de la tabla Quiz
+exports.Comment = Comment;
 
 //sequelize.sync() crea e inicializa la tabla de preguntas en DB
 sequelize.sync().then(function(){
